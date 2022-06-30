f7509825787f25d04151a702237bf6aea2b07471
Karl <karlsmith@bouzou.com>
2022-06-30 00:12:19 -0400

    Moves configs to separate jsons file

---
+++ config.js
@@ -0,0 +1,21 @@
+viewConfigJson = `{
+  "pixelSize": 8,
+  "drawVelocity": false,
+  "denDiffOptimize": true,
+  "_denDiffReq": 0.02,
+  "denDiffReq": 0.001
+}`;
+
+physicsConfigJson = `{
+  "dt": 0.002,
+  "visc": 20,
+  "diff": 20,
+  "denVelRatio": 0.45,
+  "gravity": 0.98
+}`;
+
+controllerConfigJson = `{
+  "trackPerformance": true,
+  "granularPerformance": true,
+  "debug": true
+}`;
